66dbdafec9ad80221f0d59442f66495cfec22c5d
adammantium44-jpg <adammantium44@gmail.com>
2026-05-14 22:03:50 +0700

    Update SKSEPluginLoad to log custom message
    Replaced example console message with custom load message.

--- plugin.cpp
+++ plugin.cpp
@@ -1,12 +1,12 @@
-SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+#include "PCH.h"
+
+SKSEPluginLoad(const SKSE::LoadInterface* skse)
+{
     SKSE::Init(skse);
 
-    // This example prints "Hello, world!" to the Skyrim ~ console.
-    // To view it, open the ~ console from the Skyrim Main Menu.
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
-        if (message->type == SKSE::MessagingInterface::kDataLoaded)
-            RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
-    });
+    RE::ConsoleLog::GetSingleton()->Print("EnxyAbilities Loaded!");
+
+    logger::info("EnxyAbilities Loaded!");
 
     return true;
 }
