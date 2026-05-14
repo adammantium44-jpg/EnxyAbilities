d40f763e6e0cfd7149ed0174f106532652878055
adammantium44-jpg <adammantium44@gmail.com>
2026-05-14 22:54:42 +0700

    Remove console log statements from plugin load

--- plugin.cpp
+++ plugin.cpp
@@ -4,9 +4,18 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
     SKSE::Init(skse);
 
-    RE::ConsoleLog::GetSingleton()->Print("EnxyAbilities Loaded!");
-
-    SKSE::log::info("EnxyAbilities Loaded!");
-
     return true;
 }
+
+SKSEPluginVersionData SKSEPlugin_Version = [] {
+    SKSEPluginVersionData v{};
+
+    v.PluginVersion(1);
+    v.PluginName("EnxyAbilities");
+    v.AuthorName("Enxy");
+
+    v.UsesAddressLibrary(true);
+    v.HasNoStructUse(true);
+
+    return v;
+}();
