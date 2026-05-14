d28c4437e91e37fbc20a9778ea57769736aa399f
adammantium44-jpg <adammantium44@gmail.com>
2026-05-14 23:45:35 +0700

    Remove SKSEPluginInfo definition from plugin.cpp

--- plugin.cpp
+++ plugin.cpp
@@ -1,13 +1,5 @@
 #include "PCH.h"
 
-SKSEPluginInfo(
-    .Version = { 1, 0, 0, 0 },
-    .Name = "EnxyAbilities",
-    .Author = "Enxy",
-    .SupportEmail = "none",
-    .RuntimeCompatibility = SKSE::VersionIndependence::AddressLibrary
-);
-
 SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
     SKSE::Init(skse);
